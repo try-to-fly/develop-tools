a47c8863c395b4456f37559fb44913a286153ab9
王宁 <wangning@shuwen.com>
2022-12-16 20:42:05 +0800

    chore: 增加对vercel的支持

--- remix.config.js
+++ remix.config.js
@@ -2,6 +2,8 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
+  serverBuildTarget: "vercel",
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
 };

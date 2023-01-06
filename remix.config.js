f4d764c10a4aad4e589411616f7b522ba4ba9b28
王宁 <wangning@shuwen.com>
2023-01-06 12:28:09 +0800

    chore: 尝试支持fly部署

--- remix.config.js
+++ remix.config.js
@@ -1,10 +1,17 @@
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
-module.exports = {
-  serverBuildTarget: "vercel",
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
-  cacheDirectory: "./node_modules/.cache/remix",
-  ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
-  serverDependenciesToBundle: []
-};
+module.exports = process.env.VERCEL_ENV
+  ? {
+      serverBuildTarget: "vercel",
+      server:
+        process.env.NODE_ENV === "development" ? undefined : "./server.js",
+      cacheDirectory: "./node_modules/.cache/remix",
+      ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
+      serverDependenciesToBundle: [],
+    }
+  : {
+      cacheDirectory: "./node_modules/.cache/remix",
+      ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
+      serverDependenciesToBundle: [],
+    };
